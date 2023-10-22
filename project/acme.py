d71648eb7c595ed5d9b108d7e62998a096a67651
kwokwailui <klui@ethz.ch>
2023-10-22 05:43:05 +0200

    second push to test, I don't know what I'm pushing tho

--- project/acme.py
+++ project/acme.py
@@ -1,21 +1,6 @@
 """
+Acme client implementation
 
-
-
-Allowed libraries:
-cryptography
-click
-dacite
-Django
-dnslib
-Flask
-falcon
-gunicorn
-PyCryptodome
-pycrypto
-pyopenssl
-requests
-tornado
 
 
 """
@@ -24,85 +9,37 @@ from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography import x509
 from cryptography.x509.oid import NameOID
 from cryptography.hazmat.primitives import hashes
-import json
-import requests
+import argparse
+import http.server as http_server
 import dnslib
-import http.server as httpserver
+from dnslib.server import DNSServer
 import ssl
-import flask
+from flask import Flask
 import socket
 
 #Used to generate the public private key pair to prove the client is controlling it
 
 
 
-"""
-A JWS object sent as the body of an ACME request MUST meet the
-following additional criteria:
-
-o  The JWS MUST be in the Flattened JSON Serialization [RFC7515]
-
-o  The JWS MUST NOT have multiple signatures
-
-o  The JWS Unencoded Payload Option [RFC7797] MUST NOT be used
-
-o  The JWS Unprotected Header [RFC7515] MUST NOT be used
-
-o  The JWS Payload MUST NOT be detached
-
-o  The JWS Protected Header MUST include the following fields:
-
-  *  "alg" (Algorithm)
-
-     +  This field MUST NOT contain "none" or a Message
-        Authentication Code (MAC) algorithm (e.g. one in which the
-        algorithm registry description mentions MAC/HMAC).
-
-  *  "nonce" (defined in Section 6.5)
-
-  *  "url" (defined in Section 6.4)
-
-  *  Either "jwk" (JSON Web Key) or "kid" (Key ID) as specified
-     below
-     
-     
-Used for message size transport and convert the message into UTF-8
-
-"""
-class JWS:
-    def __init__(self):
-        pass
-    def encapsulationJWS(self, data):
-        pass
-
-
-
-    def convertUTF8(self, data):
-        #Converts the data into UTF-8
-        return data.encode('utf-8')
-
 
 class Certificate_HTTPS:
-    def __init__(self):
-        self.key = self.generateKey()
-        self.Certificate = x509()
-        self.HTTP_server = 5002 #Challenge HTTP server
-        self.HTTPS_server = 5001 #Certificate HTTP server
-        self.HTTPS_shutDown = 5003 #Certificate HTTP server shutdown
+    def __init__(self,ip, port):
+        self.IP = ip
+        self.port = port
 
 
-    def startHTTPServer(self):
-        #Starts the HTTP server
-
-        pass
-
-    def GenerateCSRForServer(self, key):
+    def GenerateCSRForServer(self):
+        #Generate the key for the server
+        self.key = rsa.generate_private_key(
+            public_exponent=65537,
+            key_size=2048
+        )
         # Generate a CSR
-        csr = x509.CertificateSigningRequestBuilder().subject_name(x509.Name([
+        self.csr = x509.CertificateSigningRequestBuilder().subject_name(x509.Name([
             # Provide various details about who we are.
             x509.NameAttribute(NameOID.COUNTRY_NAME, u"CH"), #Country
             x509.NameAttribute(NameOID.STATE_OR_PROVINCE_NAME, u"Zurich"), #State
-            x509.NameAttribute(NameOID.LOCALITY_NAME, u"PfaeffikonZH"), #Locality
+            x509.NameAttribute(NameOID.LOCALITY_NAME, u"Zurich"), #Locality
             x509.NameAttribute(NameOID.ORGANIZATION_NAME, u"NetSecProject"), #Organization
             x509.NameAttribute(NameOID.COMMON_NAME, u"newtwitter.ch"), #Common name
         ])).add_extension(
@@ -112,18 +49,13 @@ class Certificate_HTTPS:
             ]),
             critical=False,
             # Sign the CSR with the private key.
-        ).sign(key, hashes.SHA256())
-        return csr
+        ).sign(self.key, hashes.SHA256())
+        return self.key, self.csr
 
     def stopHTTPServer(self):
         #Stops the HTTP server
         pass
 
-    def GenerateRSAKeyPair(self):
-        return rsa.generate_private_key(
-            public_exponent=65537,
-            key_size=2048
-        )
 
 
 
@@ -133,62 +65,30 @@ class DNS_Server:
     """
     Setup my own dns server
 
-    Domain name: newtwitter.ch
-
     """
-    def __init__(self, HTTP_resolver):
-        self.dnsport = 10053 #DNS port on specification
-        self.domain = "newtwitter.ch" #Domain name
-        self.ipAddr = "127.0.0.1"
-        self.server = dnslib.DNSServer(
-            HTTP_resolver,
-            port=self.dnsport,
-            address=self.ipAddr
-        )
-        self.SOA_record = dnslib.SOA(
-            mname="ns1.newtwitter.ch", # primary name server
-            rname="admin.newtwitter.ch", # email of admin
-            times=(
-                2017010101,  # serial number
-                60,  # refresh
-                60,  # retry
-                60,  # expire
-                60,  # minimum
-            )
-        )
+    def __init__(self, request, handler, record):
+        self.request = request
+        self.handler = handler
+        self.record = record
 
-    def startServer(self):
-        pass
+    def dns_resolve(self):
+        reply = self.request.reply()
+        qType = self.request.q
+        q_name = self.request.q.qname
 
-    def stopServer(self):
-        pass
+        print("qType: ", qType)
+        print("q_name: ", q_name)
+        print("q: ", self.request.q)
+        print("qname: ", self.request.q.qname)
 
-    def getRecord(self):
-        return self.SOA_record[dnslib.NS("ns1.newtwitter.ch"), dnslib.NS("ns2.newtwitter.ch")]
+        if qType == dnslib.QTYPE.A:
+            reply.add_answer(dnslib.RR(q_name, dnslib.QTYPE.A, rdata=dnslib.A("localhost"), ttl=60))
 
-    def getHTTPChallenge(self, challenge):
-        #Get the HTTP challenge
-        print("Challenge: ", challenge)
-        if (challenge == "http-01"):
-            return self.getHTTPChallengeRecord() #Get the HTTP challenge record
-        elif (challenge == "dns-01"):
-            return self.getDNSChallengeRecord() #Get the DNS challenge record
+        elif qType == dnslib.QTYPE.TXT:
+            reply.add_answer(dnslib.RR(q_name, dnslib.QTYPE.TXT, rdata=dnslib.TXT("Hello World"), ttl=60))
         else:
-            raise Exception("Challenge not supported")
-
-    def getChallenge(self):
-        pass
-
-    def setChallenge(self):
-        pass
-
-    def getChallengeRecord(self):
-        pass
-
-    def setChallengeRecord(self):
-        pass
-
-
+            reply.add_answer(dnslib.RR(q_name, dnslib.QTYPE.CNAME, rdata=dnslib.CNAME("localhost"), ttl=60))
+        return reply
 
 
 
@@ -196,33 +96,108 @@ class DNS_Server:
 """
 Main implementation of the ACME client
 -------------------------------------------
+                                  directory
+                                      |
+                                      +--> newNonce
+                                      |
+          +----------+----------+-----+-----+------------+
+          |          |          |           |            |
+          |          |          |           |            |
+          V          V          V           V            V
+     newAccount   newAuthz   newOrder   revokeCert   keyChange
+          |          |          |
+          |          |          |
+          V          |          V
+       account       |        order --+--> finalize
+                     |          |     |
+                     |          |     +--> cert
+                     |          V
+                     +---> authorization
+                               | ^
+                               | | "up"
+                               V |
+                             challenge
 
+                     ACME Resources and Relationships
 """
 class ACME_Client:
     def __init__(self):
         pass
 
-    def setupServer(self):
+    def AccountCreation(self):
+        #Create a new account and return account
         pass
 
-    def placeCertOrder(self):
+    def newOrder(self):
         pass
 
-    def checkChallengeValid(self, challenge):
+    def revokeCert(self):
         pass
 
-
-    def placeCSR(self):
-        pass
-
-    def getCert(self):
+    def keyChange(self):
         pass
 
 
 
+def keyAuthorization(token, accountkey):
+    return token + "." + accountkey
 
-cert = Certificate_HTTPS()
-key = cert.GenerateRSAKeyPair()
-csr = cert.GenerateCSRForServer(key)
-print("Key: ", key)
-print("CSR: ", csr)
+
+
+def main():
+    parse = argparse.ArgumentParser(description='ACME client')
+    parse.add_argument('-u', '--dir', help='The directory you want to get certified', required=True)
+    parse.add_argument('-c', '--record', help='Only challenge type', required=True)
+    parse.add_argument('-d', '--domain', help='The domain you want to access.', action='append', required=True)
+    parse.add_argument('-r', '--certificate',help='certificate', required=False)
+    args = parse.parse_args()
+
+    DNS_SERVER_PORT = 10053 #UDP port 10053
+    CHALLENGE_SERVER_PORT = 5002 #TCP port 5002
+    CHALLENGE_SERVER_SHUTDOWN_PORT = 5003 #TCP port 5003
+    CERTIFICATE_PORT = 5001 #TCP port 5001
+
+    #Start the DNS server
+    resolver = dnslib.server.DNSResolver()
+    server = DNSServer(resolver,port=DNS_SERVER_PORT,address = "localhost",logger = dnslib.DNSLogger(prefix = False), tcp=False)
+    server.start_thread()
+    assert server.is_running
+    print("DNS server started")
+
+    #Start the HTTP server
+    app = Flask(__name__)
+    @app.route('/.well-known/acme-challenge/<token>', methods=['GET'])
+    def challenge(token):
+        return token
+    app.run(host='localhost', port=CHALLENGE_SERVER_PORT, debug=True)
+    print("HTTP server started")
+
+    #Start the HTTPS server
+    context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+
+    #Generate the key and the certificate
+    key, csr = Certificate_HTTPS().GenerateCSRForServer()
+    context.load_cert_chain(csr, key)
+    context.check_hostname = False
+    context.verify_mode = ssl.CERT_NONE
+    #Start the HTTPS server
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+        sock.bind(('localhost',CERTIFICATE_PORT))
+        sock.listen(5)
+        with context.wrap_socket(sock, server_side=True) as ssock:
+            conn, addr = ssock.accept()
+            with conn:
+                print('Connected by', addr)
+                while True:
+                    data = conn.recv(1024)
+                    if not data: break
+                    conn.sendall(data)
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
+
