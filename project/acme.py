fffd0463116e9d6dfc8b26d5478635c1ede10ed9
kwokwailui <klui@ethz.ch>
2023-11-09 16:47:47 +0100

    added mod

--- project/acme.py
+++ project/acme.py
@@ -903,8 +903,8 @@ def main():
     server = requests.Session()
     #server.verify = False
     #server.verify = 'pebble.minica.pem'
-    #root_ca = 'pebble.minica.pem'
-    root_ca = False
+    root_ca = 'pebble.minica.pem'
+    #root_ca = False
     server.verify= root_ca
     #server_response = server.get(args.dir, verify = 'pebble.minica.pem')
     #print(server_response.json())
